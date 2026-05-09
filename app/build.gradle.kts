0a7c79404faedbb87e698093285b96b23a115594
hsikkk <gcorp14@naver.com>
2026-05-09 11:41:08 +0900

    chore: bump version to 1.8.0 (30) - minor release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.muuu.unshort"
         minSdk = 26
         targetSdk = 35
-        versionCode = 29
-        versionName = "1.7.0"
+        versionCode = 30
+        versionName = "1.8.0"
     }
 
     buildTypes {
