2b516d0d9630d12feb8db7a3703b3f1a641bed42
muuu <muuu@namhyeonsig-ui-Macmini.local>
2026-09-17 21:53:04 +0900

    chore: release 1.12.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.muuu.unshort"
         minSdk = 26
         targetSdk = 36
-        versionCode = 37
-        versionName = "1.12.0"
+        versionCode = 38
+        versionName = "1.12.1"
     }
 
     buildTypes {
